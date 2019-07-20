ebcb5a6bf4085c6d1daa85252ee419ed406f3373
artpop6 <2017118621@qq.com>
2019-07-20 11:52:37 +0800

    import

---
+++ js/ES-module-a/main.js
@@ -0,0 +1,8 @@
+export default {
+    a: 1,
+    b: 2
+}
+// default export 默认导出
+// name export 
+export const a = 1;
+export const b = 3;
---
+++ js/ES-module-a/middle.js
@@ -0,0 +1,7 @@
+// import { b } from './main.js';
+// export const b = b;
+
+// 缩写为
+export { b as d} from './main.js';
+export { default as cc } from './main.js';
+export * from './main.js';
